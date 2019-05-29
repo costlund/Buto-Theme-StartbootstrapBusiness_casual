fcb58eb4fcf579d08ac4287e3f93f2e3977e5328
Claes Ö <claes@stenbergit.se>
2019-05-29 07:41:02 +0200

    Store script.

--- public/ThemeStartbootstrapBusiness_casual.js
+++ public/ThemeStartbootstrapBusiness_casual.js
@@ -3,6 +3,10 @@ function ThemeStartbootstrapBusiness_casual(){
     $('li.active').removeClass('active');    
     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
   }
+  this.store_set_time = function(){
+    $(".list-hours li").eq(new Date().getDay()).addClass("today");    
+  }
 }
 var ThemeStartbootstrapBusiness_casual = new ThemeStartbootstrapBusiness_casual();
 ThemeStartbootstrapBusiness_casual.navbar_set_active();
+ThemeStartbootstrapBusiness_casual.store_set_time();
